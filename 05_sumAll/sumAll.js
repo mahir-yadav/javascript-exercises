110eba0840f71aa34144c3d9e2336254fb974ae8
mahir <mahir21263@iiitd.ac.in>
2025-03-16 03:16:23 +0530

    updated

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const isPositiveInteger = function (a) {
+    return typeof a == "number" && a >= 0 && Number.isInteger(a);
+}
+const sumAll = function (a, b) {
+    if (isPositiveInteger(a) && isPositiveInteger(b)) {
+        l = Math.min(a, b);
+        r = Math.max(a, b);
+        let sum = 0
+        for (let i = l; i <= r; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
