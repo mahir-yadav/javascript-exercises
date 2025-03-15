878d3adf2b5bf76ef29b6203f587a193ef548058
mahir <mahir21263@iiitd.ac.in>
2025-03-16 02:44:14 +0530

    updated

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (s) {
+    let rev = ""
+    for (let i = s.length - 1; i >= 0; i--) {
+        rev = rev + s[i];
+    }
+    return rev;
 };
 
 // Do not edit below this line
