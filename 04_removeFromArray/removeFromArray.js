26c8079ec1d8d26972c26cd2c57a7deabbf9912c
mahir <mahir21263@iiitd.ac.in>
2025-03-16 03:07:56 +0530

    updated

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...rem) {
+    // rem.forEach(x => {
+    //     for (let i = arr.length - 1; i >= 0; i--) {
+    //         if (arr[i] === x) {
+    //             arr.splice(i, 1);
+    //         }
+    //     }
+    // });
+
+    // return arr;
+    return arr.filter(ele => !rem.includes(ele));
+
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
