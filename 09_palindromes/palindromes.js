aac436672e912e3f198be55321225de8f480ee7f
mahir <mahir21263@iiitd.ac.in>
2025-03-21 18:37:09 +0530

    remove skip and corrected code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    const alpha = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const t = s.toLowerCase().split('').filter((c) => alpha.includes(c)).join('');
+    const rev = t.split('').reverse().join('');
+    return t === rev;
 };
 
 // Do not edit below this line
