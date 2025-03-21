67b494d0783015b64b6ec7daf1e6a62cd9f0acf8
mahir <mahir21263@iiitd.ac.in>
2025-03-21 18:26:40 +0530

    remove skip and corrected code

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const subtract = function (a, b) {
+  return a - b;
 
 };
 
-const power = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, cur) => { return total + cur }, 0)
+
+
 };
 
-const factorial = function() {
-	
+const multiply = function (arr) {
+  return arr.reduce((total, cur) => { return total * cur }, 1)
+
+};
+
+const power = function (a, b) {
+  return Math.pow(a, b);
+};
+
+const factorial = function (a) {
+  let ans = 1;
+  for (let i = 1; i <= a; i++) {
+    ans *= i;
+  }
+  return ans;
 };
 
 // Do not edit below this line
