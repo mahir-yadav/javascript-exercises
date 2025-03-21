3edb3d68d6e4a36f3b63085c3cb75637310a1645
mahir <mahir21263@iiitd.ac.in>
2025-03-21 18:55:11 +0530

    remove skip and corrected code

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    if (num < 0) return "OOPS";
+    num = parseInt(num);
+    if (num == 0) return 0;
+    let cur = 1;
+    let pre = 0;
+    for (let i = 0; i < num - 1; i++) {
+        let x = cur;
+        cur = cur + pre;
+        pre = x;
+    }
+    return cur;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+// 0 1 1 2 3 5 8
