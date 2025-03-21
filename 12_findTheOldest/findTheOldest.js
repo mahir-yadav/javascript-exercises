9ec741a24ce671d6f99b7ed642712accc6db08f1
mahir <mahir21263@iiitd.ac.in>
2025-03-21 19:11:35 +0530

    removed skip and corrected code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+function getAge(yob, yod) {
+    if (!yod) {
+        yod = 2024;
+    }
+    return yod - yob;
+}
+const findTheOldest = function (people) {
+    return people.reduce((oldest, cur_person) => {
+        const old_age = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const cur_age = getAge(cur_person.yearOfBirth, cur_person.yearOfDeath);
+        if (old_age < cur_age) {
+            oldest = cur_person;
+        }
+        return oldest;
+    });
 };
 
 // Do not edit below this line
